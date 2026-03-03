c5dc94c12e49553f82d679ec7d0a43d0b42dec17
Stanislav <stasprudyvus8@gmail.com>
2026-03-03 15:55:37 +0100

    v0.1 Added 3 commands and started creating architecture

---
+++ expense_tracker.py
@@ -0,0 +1,10 @@
+def load_data():
+    print("Printed all expenses")
+
+def add_expense(amount, category):
+    print("Added new expense")
+    print(f"Amount: {amount}$")
+    print(f"Category: {category}")
+
+def delete_expense(expenseID):
+    print(f"Deleted expense with ID={expenseID}")
--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("Hello world")
+import argparse
+import expense_tracker
+
+#List of allowed categories
+allowed_categories = ["grocery", "eating out", "fitness", "health"]
+
+#---CREATING MAIN PARSER---
+parser = argparse.ArgumentParser("Expence tracker")
+
+#---CREATING SUB PARSERS---
+subparsers = parser.add_subparsers(dest="command", help= "Commads")
+
+#---LOAD_DATA---
+subparsers.add_parser("load_data", help = "Show all expenses")
+
+#---ADD EXPENSE---
+parser_add = subparsers.add_parser("add_expense")
+parser_add.add_argument("amount", 
+                        help = "Amount of money", 
+                        type = float)
+parser_add.add_argument("category",
+                         help = "Category of expense",
+                         choices = allowed_categories)
+
+#---DELETE EXPENSE---
+parser_delete = subparsers.add_parser("delete_expense")
+parser_delete.add_argument("expenseID",
+                            help = "ID of expense", 
+                            type = int)
+
+#Getting arguments to args
+args = parser.parse_args()
+
+#Calling functions from expese_tracker.py 
+if args.command == "load_data":
+    expense_tracker.load_data()
+
+elif args.command == "add_expense":
+    expense_tracker.add_expense(args.amount, args.category)
+
+elif args.command == "delete_expense":
+    expense_tracker.delete_expense(args.expenseID)
